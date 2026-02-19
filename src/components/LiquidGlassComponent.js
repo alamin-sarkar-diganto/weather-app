48d2a30da6ad2666996c64c91a5f2379395b2c42
alamin-sarkar-diganto <alamin.sarkar.diganto@gmail.com>
2026-02-19 11:05:46 +0600

    Create LiquidGlassComponent.js with liquid glass UI styling component.

---
+++ src/components/LiquidGlassComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LiquidGlassComponent.css';
+
+const LiquidGlassComponent = () => {
+    return (
+        <div className="liquid-glass">
+            <h1>Your Liquid Glass Component</h1>
+        </div>
+    );
+};
+
+export default LiquidGlassComponent;
